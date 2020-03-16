8fc3129293e9c43b5fcf067d1fde56b0d6a0c8a7
Francis Aboud <francisaboud2000@gmail.com>
2020-03-16 13:21:04 +0200

    handeld requsts for signIn an LogIn

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('./auth');
 
-// router.get();
-// router.post();
+router.post('/api/authenticate', auth.authenticate);
+router.post('/api/addUser', auth.addUser);
+
+
+
 
 module.exports = router;
